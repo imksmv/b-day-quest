8ba5c45d8bd6b6057d6b6b9ffa6cb3f41a10b962
Illia Maksymov <98290303+imksmv@users.noreply.github.com>
2024-10-08 06:52:30 +0100

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import {
   InputOTPSeparator,
   InputOTPSlot,
 } from "@/components/ui/input-otp";
+import { Volume1 } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useRef, useState } from "react";
@@ -44,7 +45,15 @@ export default function Home() {
   return (
     <div className="px-4 flex items-center h-full justify-center">
       <div className="flex flex-col gap-6 items-center justify-center">
-        <div>
+        <div className="flex flex-col items-center">
+          <p className="text-xl">Did you find the Pass Key? :)</p>
+          <div className="flex gap-2 mb-4 items-center text-muted-foreground">
+            <Volume1 />
+            <p className="text-lg">
+              Turn on the sound, for a better experience :)
+            </p>
+          </div>
+
           {error && (
             <p className="flex items-center justify-center text-destructive">
               {error}
